0e5e7eb4559254b195bf32722dbea968729abc7e
NagyVikt <nagy.viktordp@gmail.com>
2026-01-15 16:49:35 +0100

    new

---
+++ start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Check if script is run as root
+if [ "$EUID" -ne 0 ]; then
+  echo "This script must be run as root. Try: sudo $0"
+  exit 1
+fi
+
+# Load all images
+docker load -i redis_8.4.0.tar
+docker load -i krosy-backend_image.tar
+docker load -i tabpack-controller_image.tar
+
+# Start compose
+docker compose up
